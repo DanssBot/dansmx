b8cfbe924faf9176125aecd4f263123c3d817ef6
DanssBot <90471487+DanssBot@users.noreply.github.com>
2022-09-20 15:37:24 -0500

    Update botteste.sh

--- Modulos/botteste.sh
+++ Modulos/botteste.sh
@@ -14,21 +14,21 @@
     echo ""
     echo -e "\033[1;32mINICIANDO BOT TESTE \033[0m\n"
     cd $HOME/BOT
-    rm -rf $HOME/BOT/botteste
-    wget https://www.dropbox.com/s/8bg3u09xupwzrmj/botteste >/dev/null 2>&1
-    chmod 777 botteste
+    rm -rf $HOME/BOT/botssh
+    wget https://www.dropbox.com/s/y6rd44hmu4tsw8e/botssh >/dev/null 2>&1
+    chmod 777 botssh
     echo ""
     sleep 1
-    sed -i "s/!#bvindo#!/$bvindo/g" $HOME/BOT/botteste >/dev/null 2>&1
-    sed -i "s/!#mfinal#!/$mfinal/g" $HOME/BOT/botteste >/dev/null 2>&1
-    sed -i "s/!#bt1#!/$bt1/g" $HOME/BOT/botteste >/dev/null 2>&1
-    sed -i "s/!#bt2#!/$bt2/g" $HOME/BOT/botteste >/dev/null 2>&1
-    sed -i "s/!#link2#!/$link2/g" $HOME/BOT/botteste >/dev/null 2>&1
-    sed -i "s/!#bt3#!/$bt3/g" $HOME/BOT/botteste >/dev/null 2>&1
-    sed -i "s/!#link3#!/$link3/g" $HOME/BOT/botteste >/dev/null 2>&1
-    sed -i "s/!#dtempo#!/$dtempo/g" $HOME/BOT/botteste >/dev/null 2>&1
+    sed -i "s/!#bvindo#!/$bvindo/g" $HOME/BOT/botssh >/dev/null 2>&1
+    sed -i "s/!#mfinal#!/$mfinal/g" $HOME/BOT/botssh >/dev/null 2>&1
+    sed -i "s/!#bt1#!/$bt1/g" $HOME/BOT/botssh >/dev/null 2>&1
+    sed -i "s/!#bt2#!/$bt2/g" $HOME/BOT/botssh >/dev/null 2>&1
+    sed -i "s/!#link2#!/$link2/g" $HOME/BOT/botssh >/dev/null 2>&1
+    sed -i "s/!#bt3#!/$bt3/g" $HOME/BOT/botssh >/dev/null 2>&1
+    sed -i "s/!#link3#!/$link3/g" $HOME/BOT/botssh >/dev/null 2>&1
+    sed -i "s/!#dtempo#!/$dtempo/g" $HOME/BOT/botssh >/dev/null 2>&1
     sleep 1
-    screen -dmS bot_teste ./botteste $token > /dev/null 2>&1
+    screen -dmS bot_teste ./botssh $token > /dev/null 2>&1
     clear
     echo "BOT ATIVADO"
     menu
